7cbc623747b9f7e96d1f1190ddef14f8564d7299
Jingtian Zhang <jtz9317@gmail.com>
2021-01-19 08:36:17 -0800

    commits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello From the World!</h1>
+      <input />
+      <button> Send Messages </button>
+
+      {/*input filed*/}
+      {/*button*/}
+      {/*message themselves*/}
+
     </div>
   );
 }
